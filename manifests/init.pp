e69fbe9a5e253943ded8c847a1590fab86fc9dd7
Bill Glick <bglick@illinois.edu>
2019-05-30 11:59:21 -0500

    Clean yum repo after updating it

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,21 +11,20 @@ class lsst_pipeline_prereq (
 ) {
 
   ensure_resource('yumrepo', 'centos-sclo-rh', {
-    'ensure' => 'present',
-    'baseurl' => $scl_yumrepo_baseurl,
-    'descr' => 'CentOS-7 - SCLo rh',
-    'enabled' => 1,
+    'ensure'   => 'present',
+    'baseurl'  => $scl_yumrepo_baseurl,
+    'descr'    => 'CentOS-7 - SCLo rh',
+    'enabled'  => 1,
     'gpgcheck' => 0,
-#    'gpgkey' => $scl_yumrepo_gpgkeyurl,
-#    'notify' => "Exec['clean centos-sclo-rh yum repo']",
+#    'gpgkey'   => $scl_yumrepo_gpgkeyurl,
   })
 
   exec { "clean centos-sclo-rh yum repo":
     subscribe   => Yumrepo['centos-sclo-rh'],
     refreshonly => true,
-    path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-    command => 'yum clean all --disablerepo=* --enablerepo=centos-sclo-rh',
-    require => [
+    path        => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+    command     => 'yum clean all --disablerepo=* --enablerepo=centos-sclo-rh',
+    require     => [
       Yumrepo['centos-sclo-rh'],
     ],
   }
